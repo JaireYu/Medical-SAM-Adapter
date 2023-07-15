0e6373a98b340d27c7e33d2cd3e132f1fec873b5
JaireYu <1561840701@qq.com>
2023-07-15 08:33:20 +0000

    add test sam baseline eval code

---
+++ test_sam_baseline.py
@@ -0,0 +1,109 @@
+# train.py
+#!/usr/bin/env	python3
+
+""" train network using pytorch
+    Junde Wu
+"""
+
+import os
+import sys
+import argparse
+from datetime import datetime
+from collections import OrderedDict
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import roc_auc_score, accuracy_score,confusion_matrix
+import torchvision
+import torchvision.transforms as transforms
+from skimage import io
+from torch.utils.data import DataLoader
+#from dataset import *
+from torch.autograd import Variable
+from PIL import Image
+from tensorboardX import SummaryWriter
+#from models.discriminatorlayer import discriminator
+from dataset import *
+from conf import settings
+import time
+import cfg
+from tqdm import tqdm
+from torch.utils.data import DataLoader, random_split
+from segment_anything import sam_model_registry, SamPredictor
+from utils import *
+import function 
+
+def setup_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+
+setup_seed(0)
+
+args = cfg.parse_args()
+
+GPUdevice = torch.device('cuda', args.gpu_device)
+
+sam_checkpoint = "/home/yujr/workstation/pretrain_checkpoints/sam_vit_b_01ec64.pth"
+model_type = "vit_b"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=GPUdevice)
+
+args.path_helper = set_log_dir('logs', args.exp_name)
+logger = create_logger(args.path_helper['log_path'])
+logger.info(args)
+
+
+'''segmentation data'''
+transform_train = transforms.Compose([
+    transforms.Resize((args.image_size,args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_train_seg = transforms.Compose([
+    transforms.Resize((args.out_size,args.out_size)),
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.Resize((args.image_size, args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_test_seg = transforms.Compose([
+    transforms.Resize((args.out_size,args.out_size)),
+    transforms.ToTensor(),
+])
+
+
+if args.dataset == 'isic':
+    '''isic data'''
+    isic_train_dataset = ISIC2016(args, args.data_path, transform = transform_train, transform_msk= transform_train_seg, mode = 'Training')
+    isic_test_dataset = ISIC2016(args, args.data_path, transform = transform_test, transform_msk= transform_test_seg, mode = 'Test')
+
+    nice_train_loader = DataLoader(isic_train_dataset, batch_size=args.b, shuffle=True, num_workers=8, pin_memory=True)
+    nice_test_loader = DataLoader(isic_test_dataset, batch_size=args.b, shuffle=False, num_workers=8, pin_memory=True)
+    '''end'''
+
+elif args.dataset == 'building_v1':
+    '''buiding data'''
+    isic_train_dataset = Building_v1(args, args.data_path, transform = transform_train, transform_msk= transform_train_seg, mode = 'Training')
+    isic_test_dataset = Building_v1(args, args.data_path, transform = transform_test, transform_msk= transform_test_seg, mode = 'Test')
+
+    nice_train_loader = DataLoader(isic_train_dataset, batch_size=args.b, shuffle=True, num_workers=8, pin_memory=True)
+    nice_test_loader = DataLoader(isic_test_dataset, batch_size=args.b, shuffle=False, num_workers=8, pin_memory=True)
+    '''end'''
+
+elif args.dataset == 'decathlon':
+    nice_train_loader, nice_test_loader, transform_train, transform_val, train_list, val_list =get_decath_loader(args)
+
+'''begain training'''
+best_acc = 0.0
+best_tol = 1e4
+threshold = (0.1, 0.3, 0.5, 0.7, 0.9)
+tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, 0, threshold, sam, None)
+logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {0}.')
